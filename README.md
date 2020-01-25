ef7233927fbfe95a8a9d8f5cb8984580670caeb8
boccia <fboccacini@gmail.com>
2020-01-25 15:34:57 +0900

    hex2chr v1.1

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # python-utils
 random python utilities
 
-hex2chr.py: takes a hex space separated strings as argument, or asks for it, and converts it to an ascii string
+hex2chr.py: takes a hex space separated strings as argument, or asks for it, and converts it to an ascii string.
+            Usually prints out a conversion chart, use with -s to print just print the final string, with no 
--- hex2chr.py
+++ hex2chr.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python
 
-# Hex to char converter
+# Hex to char converter, version 1.1
 
 import sys
+
+verbose = True
 
 if len(sys.argv) < 2:
     string = input('Hex string (space separated): ')
     splt = string.split(' ')
 else:
-    splt = sys.argv[1:]
+    splt = []
+    for opt in sys.argv:
 
-print('')
+        try:
+            chr(int(opt,16))
+            splt.append(opt)
+        except:
+            if opt == '-s':
+                verbose = False
 
 new_str = ''
 
 for char in splt:
-    print(char, int(char,16), chr(int(char,16)), sep=' - ')
+    if verbose:
+        print(char, int(char,16), chr(int(char,16)), sep=' - ')
     new_str += chr(int(char,16))
-print('\n')
 
 print(new_str)
